583e4ac3ce2700fcee0135ff3dd2326077b1e9c8
Chris <chriskang0917@gmail.com>
2023-11-16 09:59:21 +0800

    Chros: ts config for production

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,17 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
