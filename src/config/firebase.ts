74bdaa359ff910a655497f627b69313077e83aab
Chris <chriskang0917@gmail.com>
2023-11-16 09:43:07 +0800

    Chore: firebase env setting

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCr-Mgci-dyXYmZQzRnuhPxYEjsrm9gqKk",
+  authDomain: "reminded-9f648.firebaseapp.com",
+  projectId: "reminded-9f648",
+  storageBucket: "reminded-9f648.appspot.com",
+  messagingSenderId: "1040697615970",
+  appId: "1:1040697615970:web:71c4a4eadcf445ccc3133b",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
