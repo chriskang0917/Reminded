1b58045caa0b0074ab797c544a82d6e2505cdf90
Chris <chriskang0917@gmail.com>
2023-11-17 15:40:41 +0800

    Feat: build card state in cardStore

--- src/store/cardStore.ts
+++ src/store/cardStore.ts
@@ -1,21 +1,83 @@
 import { makeAutoObservable } from "mobx";
+import { nanoid } from "nanoid";
+
+type cardStatus = "idea" | "action" | "todo" | "remind" | "note" | "execute";
 
 interface Card {
-  id: number;
+  id: string;
   content: string;
+  tags: string[];
+  status: cardStatus;
+  isArchived: boolean;
+  isImportant: boolean;
+  createdTime: number;
+  updatedTime: number;
+  dueDate: number | null; // null if not exist
+  reminderStartDate: number | null; // null if not exist
+  reminderEndDate: number | null; // null if not exist
 }
 
 export class CardStore {
-  number = 5;
   cards: Card[] = [];
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  increment() {
-    this.number++;
+  addCard(status: cardStatus, content: string, tags: string[]) {
+    this.cards.push({
+      id: nanoid(),
+      status,
+      content,
+      tags,
+      isArchived: false,
+      isImportant: false,
+      createdTime: Date.now(),
+      updatedTime: Date.now(),
+      dueDate: null,
+      reminderStartDate: null,
+      reminderEndDate: null,
+    });
   }
 }
 
 export const cardStore = new CardStore();
+
+// interface UserSettings {
+//   user_id: {
+//     score: number; // 100
+//     favoriteIdeaTags: string[]; // "work", "study"
+//     favoriteActionTags: string[]; // "work", "study"
+//     favoriteTodoTags: string[]; // "work", "study"
+//     ideaDailyReminders: string[]; // "9:00", "12:00", "15:00", "18:00", "21:00"
+//     startDayOfWeek: string; // "MON", "SUN"
+//     defaultDueDate: number; // 1 (意指明天)
+//     sortTypeOfAllIdeas:
+//       | "asc"
+//       | "desc"
+//       | "ascCreatedTime"
+//       | "descCreatedTime"
+//       | "ascModifiedTime"
+//       | "descModifiedTime"
+//       | "custom";
+//     sortTypeOfAllActions:
+//       | "asc"
+//       | "desc"
+//       | "ascCreatedTime"
+//       | "descCreatedTime"
+//       | "ascModifiedTime"
+//       | "descModifiedTime";
+//     sortTypeOfAllTasks:
+//       | "asc"
+//       | "desc"
+//       | "ascCreatedTime"
+//       | "descCreatedTime"
+//       | "ascModifiedTime"
+//       | "descModifiedTime"
+//       | "ascDueDate"
+//       | "descDueDate";
+//     sortTypeOfAllReminders: string; // "asc", "desc"
+//     sortTypeOfTodayTasks: string; // "asc", "desc"
+//     sortTypeOfTomorrowTasks: string; // "asc", "desc"
+//   };
+// }
