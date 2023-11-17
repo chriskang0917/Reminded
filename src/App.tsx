e5a8d3d7e0d36692fe3173566caedff91cddd981
Chris <chriskang0917@gmail.com>
2023-11-17 11:21:45 +0800

    Chore: set router path

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import RootLayout from "./components/RootLayout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <NextUIProvider>
-      <></>
+      <RouterProvider router={router}></RouterProvider>
     </NextUIProvider>
   );
 }
