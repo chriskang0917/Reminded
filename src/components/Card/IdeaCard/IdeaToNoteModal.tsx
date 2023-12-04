7ad948015cee625e86bd0a3e78c1412e9235cc8f
Chris <chriskang0917@gmail.com>
2023-12-04 10:42:46 +0800

    Refactor: refactor IdeaNoteModal to ModalEditor

--- src/components/Card/IdeaCard/IdeaToNoteModal.tsx
+++ src/components/Card/IdeaCard/IdeaToNoteModal.tsx
@@ -1,14 +1,5 @@
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalHeader,
-} from "@nextui-org/react";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { ICard, NewNote, cardStore } from "../../../store/cardStore";
-import NoteEditor from "../../Editor/NoteEditor";
+import { ICard } from "../../../store/cardStore";
+import ModalEditor from "../../Editor/ModalEditor";
 
 interface IdeaNoteModalProp {
   card: ICard;
@@ -17,90 +8,18 @@ interface IdeaNoteModalProp {
   onClose: () => void;
 }
 
-interface NoteContent {
-  noteTitle: string;
-  description: string;
-  noteHTML: string;
-  tags: string[];
-}
+export const IdeaNoteModal = (prop: IdeaNoteModalProp) => {
+  const { card, isOpen, onOpenChange, onClose } = prop;
 
-export const IdeaNoteModal = ({
-  card,
-  isOpen,
-  onOpenChange,
-  onClose,
-}: IdeaNoteModalProp) => {
-  const [noteContent, setNoteContent] = useState<NoteContent>({
-    noteTitle: "",
-    description: "",
-    noteHTML: "",
-    tags: [],
-  });
-
-  const handleNoteChange = (
-    noteTitle: string,
-    description: string,
-    noteHTML: string,
-    tags: string[],
-  ) => {
-    setNoteContent({ noteTitle, description, noteHTML, tags });
-  };
-
-  const handleSubmit = () => {
-    const note = new NewNote(
-      noteContent.noteTitle,
-      noteContent.description,
-      noteContent.noteHTML,
-      card.tags,
-    );
-    cardStore.addNote(note);
-    cardStore.addNoteToFireStore(note);
-    toast.success("已新增筆記");
-    onClose();
-  };
+  const pageTitle = "轉換你的靈感...";
 
   return (
-    <Modal
-      className="mx-10 max-w-[600px] p-4"
-      backdrop="blur"
-      hideCloseButton
-      isKeyboardDismissDisabled
-      isDismissable={false}
+    <ModalEditor
+      pageTitle={pageTitle}
+      card={card}
       isOpen={isOpen}
       onOpenChange={onOpenChange}
-    >
-      <ModalContent>
-        <ModalHeader className="flex justify-between">
-          <h1 className="tracking-wider text-third">轉換你的靈感...</h1>
-          <div className="flex gap-2">
-            <Button
-              className="min-w-[40px] tracking-wider"
-              radius="sm"
-              variant="light"
-              color="danger"
-              onPress={onClose}
-            >
-              關閉
-            </Button>
-            <Button
-              className="min-w-[40px] tracking-wider"
-              radius="sm"
-              color="primary"
-              variant="shadow"
-              onPress={handleSubmit}
-            >
-              記錄
-            </Button>
-          </div>
-        </ModalHeader>
-        <ModalBody>
-          <NoteEditor
-            card={card}
-            onClose={onClose}
-            onNoteChange={handleNoteChange}
-          />
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+      onClose={onClose}
+    />
   );
 };
---
+++ src/components/Editor/ModalEditor.tsx
@@ -0,0 +1,109 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+} from "@nextui-org/react";
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { ICard, NewNote, cardStore } from "../../store/cardStore";
+import NoteEditor from "./NoteEditor";
+
+interface IdeaNoteModalProp {
+  pageTitle?: string;
+  card: ICard;
+  isOpen: boolean;
+  onOpenChange: () => void;
+  onClose: () => void;
+}
+
+interface NoteContent {
+  noteTitle: string;
+  description: string;
+  noteHTML: string;
+  tags: string[];
+}
+
+const ModalEditor = observer((prop: IdeaNoteModalProp) => {
+  const { pageTitle, card, isOpen, onOpenChange, onClose } = prop;
+
+  const [noteContent, setNoteContent] = useState<NoteContent>({
+    noteTitle: "",
+    description: "",
+    noteHTML: "",
+    tags: [],
+  });
+
+  const handleNoteChange = (
+    noteTitle: string,
+    description: string,
+    noteHTML: string,
+    tags: string[],
+  ) => {
+    setNoteContent({ noteTitle, description, noteHTML, tags });
+  };
+
+  const handleSubmit = () => {
+    const note = new NewNote(
+      noteContent.noteTitle,
+      noteContent.description,
+      noteContent.noteHTML,
+      card.tags,
+    );
+    cardStore.addNote(note);
+    cardStore.addNoteToFireStore(note);
+    toast.success("已新增筆記");
+    onClose();
+  };
+
+  return (
+    <Modal
+      className="mx-10 max-w-[600px] p-4"
+      backdrop="blur"
+      hideCloseButton
+      isKeyboardDismissDisabled
+      isDismissable={false}
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+    >
+      <ModalContent>
+        <ModalHeader className="flex justify-between">
+          <h1 className="tracking-wider text-third">
+            {pageTitle ? pageTitle : "編輯你的筆記"}
+          </h1>
+          <div className="flex gap-2">
+            <Button
+              className="min-w-[40px] tracking-wider"
+              radius="sm"
+              variant="light"
+              color="danger"
+              onPress={onClose}
+            >
+              關閉
+            </Button>
+            <Button
+              className="min-w-[40px] tracking-wider"
+              radius="sm"
+              color="primary"
+              variant="shadow"
+              onPress={handleSubmit}
+            >
+              記錄
+            </Button>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <NoteEditor
+            card={card}
+            onClose={onClose}
+            onNoteChange={handleNoteChange}
+          />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+});
+
+export default ModalEditor;
