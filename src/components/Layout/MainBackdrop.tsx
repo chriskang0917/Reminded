6b3cafe61668bb38beb203c32b8c560e4310b475
Chris <chriskang0917@gmail.com>
2023-12-05 16:14:52 +0800

    Style: add shadow to subside bar

--- src/components/Layout/MainBackdrop.tsx
+++ src/components/Layout/MainBackdrop.tsx
@@ -1,7 +1,7 @@
 function MainBackdrop({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative z-50">
-      <div className="fixed left-0 top-0 ml-60 h-[100svh] w-full rounded-[40px] bg-white" />
+      <div className="fixed left-0 top-0 ml-60 h-[100svh] w-full rounded-[40px] bg-white shadow-xl" />
       {children}
     </div>
   );
