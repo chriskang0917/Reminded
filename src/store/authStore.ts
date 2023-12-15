64cb91d43a3399133546789cf30d0bdd63848d09
Chris <chriskang0917@gmail.com>
2023-12-15 18:48:50 +0800

    Refactor: replace auth with DI

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -22,85 +22,61 @@ interface ITutorial {
   todo: boolean;
 }
 
-interface AuthService {
+interface EmailService {
   initAuthState: () => void;
   initState: () => void;
-  initUserSettings: () => void;
   register: (email: string, password: string, callback?: () => void) => void;
   login: (email: string, password: string) => void;
   logout: () => void;
+}
+
+interface SettingService {
+  initUserSettings: () => void;
   updateProfile: (profile: IProfile) => void;
   updateTutorialProgress: (tutorialName: keyof ITutorial) => void;
 }
 
-class EmailAuthService implements AuthService {
+/* ===============================
+==========  Email Auth  ==========
+=============================== */
+
+class EmailAuthService implements EmailService {
+  public authStore: AuthStore;
   private cookieExpireDay = 10;
+
+  constructor(authStore: AuthStore) {
+    this.authStore = authStore;
+  }
 
   initAuthState() {
     if (!cookie.getCookie("uid")) return;
     auth.onAuthStateChanged((user) => {
       if (user) {
         runInAction(() => {
-          authStore.uid = user.uid;
-          authStore.isLogin = true;
+          this.authStore.uid = user.uid;
+          this.authStore.isLogin = true;
         });
       } else {
         runInAction(() => {
-          authStore.isLogin = false;
+          this.authStore.isLogin = false;
         });
       }
     });
   }
 
   initState() {
-    if (!authStore.uid) return;
-    onSnapshot(doc(db, "users", authStore.uid), (doc) => {
+    if (!this.authStore.uid) return;
+    onSnapshot(doc(db, "users", this.authStore.uid), (doc) => {
       try {
         if (!doc.exists()) throw new Error("profile_not_exist");
         runInAction(() => {
-          authStore.name = doc.data()?.name;
-          authStore.email = doc.data()?.email;
-          authStore.uid = doc.data()?.uid;
+          this.authStore.name = doc.data()?.name;
+          this.authStore.email = doc.data()?.email;
+          this.authStore.uid = doc.data()?.uid;
         });
       } catch (error) {
         console.error("init_state_error", error);
       }
-    });
-  }
-
-  initUserSettings() {
-    if (!authStore.uid) return;
-    const settingRef = doc(db, "users", authStore.uid);
-    onSnapshot(settingRef, (doc) => {
-      if (doc.data()?.tutorialProgress) {
-        return runInAction(() => {
-          authStore.tutorialProgress = doc.data()?.tutorialProgress;
-        });
-      }
-
-      localStorage.removeItem("settings");
-      const settings = localStorage.getItem("settings");
-
-      if (settings) {
-        const parsedSettings = JSON.parse(settings);
-        return runInAction(() => {
-          authStore.tutorialProgress = parsedSettings.tutorialProgress;
-        });
-      }
-
-      const initSetting = {
-        tutorialProgress: {
-          today: false,
-          idea: false,
-          action: false,
-          todo: false,
-        },
-      };
-
-      localStorage.setItem("settings", JSON.stringify(initSetting));
-      runInAction(() => {
-        authStore.tutorialProgress = initSetting.tutorialProgress;
-      });
     });
   }
 
@@ -116,19 +92,19 @@ class EmailAuthService implements AuthService {
         if (callback) callback("success");
 
         runInAction(() => {
-          authStore.uid = user?.uid;
-          authStore.isLogin = true;
+          this.authStore.uid = user?.uid;
+          this.authStore.isLogin = true;
         });
       })
       .then(() => {
-        if (!authStore.uid) return;
-        const profileRef = doc(db, "users", authStore.uid);
+        if (!this.authStore.uid) return;
+        const profileRef = doc(db, "users", this.authStore.uid);
         const randomName = `User_${Math.floor(Math.random() * 10000)}`;
-        runInAction(() => (authStore.name = randomName));
+        runInAction(() => (this.authStore.name = randomName));
 
         setDoc(profileRef, {
-          name: authStore.name,
-          uid: authStore.uid,
+          name: this.authStore.name,
+          uid: this.authStore.uid,
           email,
         });
       })
@@ -187,9 +163,9 @@ class EmailAuthService implements AuthService {
         cookie.setCookie("uid", user?.uid, this.cookieExpireDay);
 
         runInAction(() => {
-          authStore.uid = user?.uid;
-          authStore.email = user?.email;
-          authStore.isLogin = true;
+          this.authStore.uid = user?.uid;
+          this.authStore.email = user?.email;
+          this.authStore.isLogin = true;
         });
       })
       .catch((error) => {
@@ -205,17 +181,65 @@ class EmailAuthService implements AuthService {
     cookie.deleteCookie("uid");
     localStorage.removeItem("settings");
     runInAction(() => {
-      authStore.isLogin = false;
-      authStore.uid = null;
+      this.authStore.isLogin = false;
+      this.authStore.uid = null;
     });
 
     signOut(auth)
       .then(() => toast.success("登出成功"))
       .catch((error) => console.error("sign_out_error", error));
   }
+}
+
+/* ===============================
+=========  Setting Auth  =========
+=============================== */
+
+class SettingAuthService implements SettingService {
+  public authStore: AuthStore;
+
+  constructor(authStore: AuthStore) {
+    this.authStore = authStore;
+  }
+
+  initUserSettings() {
+    if (!this.authStore.uid) return;
+    const settingRef = doc(db, "users", this.authStore.uid);
+    onSnapshot(settingRef, (doc) => {
+      if (doc.data()?.tutorialProgress) {
+        return runInAction(() => {
+          this.authStore.tutorialProgress = doc.data()?.tutorialProgress;
+        });
+      }
+
+      localStorage.removeItem("settings");
+      const settings = localStorage.getItem("settings");
+
+      if (settings) {
+        const parsedSettings = JSON.parse(settings);
+        return runInAction(() => {
+          this.authStore.tutorialProgress = parsedSettings.tutorialProgress;
+        });
+      }
+
+      const initSetting = {
+        tutorialProgress: {
+          today: false,
+          idea: false,
+          action: false,
+          todo: false,
+        },
+      };
+
+      localStorage.setItem("settings", JSON.stringify(initSetting));
+      runInAction(() => {
+        this.authStore.tutorialProgress = initSetting.tutorialProgress;
+      });
+    });
+  }
 
   async updateProfile({ name, email }: IProfile) {
-    const uid = authStore.uid;
+    const uid = this.authStore.uid;
     if (!uid) return;
 
     try {
@@ -224,8 +248,8 @@ class EmailAuthService implements AuthService {
 
       toast.success("更新成功");
       runInAction(() => {
-        authStore.name = name;
-        authStore.email = email;
+        this.authStore.name = name;
+        this.authStore.email = email;
       });
     } catch (error) {
       toast.error("更新失敗");
@@ -234,23 +258,23 @@ class EmailAuthService implements AuthService {
   }
 
   updateTutorialProgress(tutorialName: keyof ITutorial) {
-    if (Object.keys(authStore.tutorialProgress || {}).length === 0) return;
+    if (Object.keys(this.authStore.tutorialProgress || {}).length === 0) return;
 
     runInAction(() => {
-      authStore.tutorialProgress[tutorialName] = true;
+      this.authStore.tutorialProgress[tutorialName] = true;
     });
 
     localStorage.setItem(
       "settings",
-      JSON.stringify({ tutorialProgress: authStore.tutorialProgress }),
+      JSON.stringify({ tutorialProgress: this.authStore.tutorialProgress }),
     );
 
-    if (!authStore.uid) return;
-    const settingRef = doc(db, "users", authStore.uid);
+    if (!this.authStore.uid) return;
+    const settingRef = doc(db, "users", this.authStore.uid);
     setDoc(
       settingRef,
       {
-        tutorialProgress: authStore.tutorialProgress,
+        tutorialProgress: this.authStore.tutorialProgress,
       },
       { merge: true },
     );
@@ -258,7 +282,9 @@ class EmailAuthService implements AuthService {
 }
 
 class AuthStore {
-  private authService: AuthService;
+  private emailService: EmailService;
+  private settingService: SettingService;
+
   public isLogin = true;
   public uid: string | null = null;
   public name: string | null = null;
@@ -268,43 +294,43 @@ class AuthStore {
   public favoriteTodoTags: string[] = [];
   public tutorialProgress: ITutorial = {} as ITutorial;
 
-  constructor(authService: AuthService) {
+  constructor() {
     makeAutoObservable(this);
-    this.authService = authService;
+    this.emailService = new EmailAuthService(this);
+    this.settingService = new SettingAuthService(this);
   }
 
   initAuthState() {
-    this.authService.initAuthState();
+    this.emailService.initAuthState();
   }
 
   async initState() {
-    this.authService.initState();
-  }
-
-  initUserSettings() {
-    this.authService.initUserSettings();
+    this.emailService.initState();
   }
 
   register(email: string, password: string, callback?: () => void) {
-    this.authService.register(email, password, callback);
+    this.emailService.register(email, password, callback);
   }
 
   login(email: string, password: string) {
-    this.authService.login(email, password);
+    this.emailService.login(email, password);
   }
 
   logout() {
-    this.authService.logout();
+    this.emailService.logout();
+  }
+
+  initUserSettings() {
+    this.settingService.initUserSettings();
   }
 
   updateProfile(profile: IProfile) {
-    this.authService.updateProfile(profile);
+    this.settingService.updateProfile(profile);
   }
 
   updateTutorialProgress(tutorialName: keyof ITutorial) {
-    this.authService.updateTutorialProgress(tutorialName);
+    this.settingService.updateTutorialProgress(tutorialName);
   }
 }
 
-const emailAuthService = new EmailAuthService();
-export const authStore = new AuthStore(emailAuthService);
+export const authStore = new AuthStore();
