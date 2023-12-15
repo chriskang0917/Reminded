15652b3c3959fc8701cc8d908473bcd4d6be4a2e
Chris <chriskang0917@gmail.com>
2023-12-15 16:12:31 +0800

    Style: reduce sidebar shadow effect

--- src/components/Sidebar/SubsideBar.tsx
+++ src/components/Sidebar/SubsideBar.tsx
@@ -6,7 +6,7 @@ interface SubsideBarProps {
 
 export const SubsideBar = observer(({ children }: SubsideBarProps) => {
   return (
-    <div className="fixed left-0 top-0 z-20 ml-20 h-[100svh] w-40 rounded-[40px] bg-fourth pt-28 shadow-xl">
+    <div className="fixed left-0 top-0 z-20 ml-20 h-[100svh] w-40 rounded-[40px] bg-fourth pt-28 shadow-md">
       <aside>
         <ul className="relative z-10 flex flex-col items-center justify-center gap-4 pt-4">
           {children}
