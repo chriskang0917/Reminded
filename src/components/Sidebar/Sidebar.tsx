85c53e6149754e3aca75b2914f0effbfcae0e65c
Chris <chriskang0917@gmail.com>
2023-12-22 16:22:16 +0800

    Refactor: refactor sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -46,70 +46,84 @@ export const Sidebar = () => {
     return pathname === "/" && page === "/";
   };
 
+  const renderLogo = () => {
+    return (
+      <li className="absolute left-[22px] top-8">
+        <Card className="h-9 w-9 rounded-full">
+          <img src={Logo} alt="logo" />
+        </Card>
+      </li>
+    );
+  };
+
+  const renderButtons = () => {
+    return actionList.map((action) => (
+      <li key={action.label}>
+        <Link to={action.path}>
+          <Tooltip
+            content={action.label}
+            showArrow
+            color="secondary"
+            placement="right"
+            delay={300}
+            closeDelay={100}
+          >
+            <Card
+              id={action?.id}
+              isBlurred
+              shadow="lg"
+              classNames={{
+                base: "h-10 w-10 rounded-xl drop-shadow-2xl",
+                body: cn(
+                  "hover:bg-primaryDark hover:text-white transition-all",
+                  isNoteHomePage(action.page)
+                    ? "bg-primaryDark text-white"
+                    : isHomePage(action.page)
+                      ? "bg-primaryDark text-white"
+                      : "bg-secondaryDark text-black",
+                ),
+              }}
+            >
+              <CardBody className="flex items-center justify-center">
+                <motion.div className="flex flex-col items-center justify-center">
+                  {action.icon}
+                </motion.div>
+              </CardBody>
+            </Card>
+          </Tooltip>
+        </Link>
+      </li>
+    ));
+  };
+
+  const renderProfileButton = () => {
+    return (
+      <Avatar
+        size="sm"
+        showFallback
+        classNames={{
+          img: "w-10 rounded-xl",
+          base: cn(
+            "h-10 w-10 rounded-xl bg-secondaryDark text-slate-600 drop-shadow-2xl",
+            "hover:bg-primaryDark hover:text-black transition-all",
+            location.pathname === "/profile" && "bg-primaryDark",
+          ),
+        }}
+        name={authStore.name || undefined}
+        fallback={<RxAvatar className="text-slate-500 h-8 w-8" />}
+      />
+    );
+  };
+
   return (
     <nav className="fixed left-0 top-0 flex h-[100svh] w-36 flex-col items-center justify-between bg-secondary pr-10 opacity-60">
       <ul className="mt-36 flex flex-col gap-7 pr-6">
-        <li className="absolute left-[22px] top-8">
-          <Card className="h-9 w-9 rounded-full">
-            <img src={Logo} alt="logo" />
-          </Card>
-        </li>
-        {actionList.map((action) => (
-          <li key={action.label}>
-            <Link to={action.path}>
-              <Tooltip
-                content={action.label}
-                showArrow
-                color="secondary"
-                placement="right"
-                delay={300}
-                closeDelay={100}
-              >
-                <Card
-                  id={action?.id}
-                  isBlurred
-                  shadow="lg"
-                  classNames={{
-                    base: "h-10 w-10 rounded-xl drop-shadow-2xl",
-                    body: cn(
-                      "hover:bg-primaryDark hover:text-white transition-all",
-                      isNoteHomePage(action.page)
-                        ? "bg-primaryDark text-white"
-                        : isHomePage(action.page)
-                          ? "bg-primaryDark text-white"
-                          : "bg-secondaryDark text-black",
-                    ),
-                  }}
-                >
-                  <CardBody className="flex items-center justify-center">
-                    <motion.div className="flex flex-col items-center justify-center">
-                      {action.icon}
-                    </motion.div>
-                  </CardBody>
-                </Card>
-              </Tooltip>
-            </Link>
-          </li>
-        ))}
+        {renderLogo()}
+        {renderButtons()}
       </ul>
       <ul className="mb-6 flex flex-col gap-4 pr-6">
         <li>
-          <Link to="/profile">
-            <Avatar
-              size="sm"
-              showFallback
-              classNames={{
-                img: "w-10 rounded-xl",
-                base: cn(
-                  "h-10 w-10 rounded-xl bg-secondaryDark text-slate-600 drop-shadow-2xl",
-                  "hover:bg-primaryDark hover:text-black transition-all",
-                  location.pathname === "/profile" && "bg-primaryDark",
-                ),
-              }}
-              name={authStore.name || undefined}
-              fallback={<RxAvatar className="text-slate-500 h-8 w-8" />}
-            />
-          </Link>
+          <Link to="/profile">{renderProfileButton()}</Link>
         </li>
       </ul>
     </nav>
