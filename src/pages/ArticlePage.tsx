65cebd49aaacc3d9554da09d1dbde1ce1c933231
Chris <chriskang0917@gmail.com>
2023-12-22 14:26:10 +0800

    Refactor: encapsule articlepage with data obj

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -6,62 +6,82 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 import { format, parseISO } from "date-fns";
-import parse from "html-react-parser";
 import { observer } from "mobx-react-lite";
 import { Navigate, useParams } from "react-router-dom";
 import CardTags from "../components/Card/CardTags";
 import ModalEditor from "../components/Editor/ModalEditor";
 import useDocTitle from "../hooks/useDocTitle";
 import { useStopShortcut } from "../hooks/useStopShortcut";
-import { cardStore } from "../store/cardStore";
+import { NewNote, cardStore } from "../store/cardStore";
+
+class Note {
+  private note: NewNote;
+
+  constructor(note: NewNote) {
+    this.note = note;
+  }
+
+  get details() {
+    return this.note;
+  }
+
+  get noteTitle() {
+    return this.note.noteTitle;
+  }
+
+  get noteHTML() {
+    return this.note.noteHTML;
+  }
+
+  get createdTime() {
+    const parsedDate = parseISO(this.note.createdTime);
+    return format(parsedDate, "yyyy-MM-dd");
+  }
+
+  get tags() {
+    return this.note.tags;
+  }
+}
 
 const ArticlePage = observer(() => {
   const { id } = useParams();
   const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
 
-  useStopShortcut(isOpen);
-
   if (!id) return <Navigate to="/notes/all" />;
+  const note = new Note(cardStore.getNoteWithId(id));
 
-  const note = cardStore.getNoteWithId(id);
+  useStopShortcut(isOpen);
+  useDocTitle(`Reminded | ${note.noteTitle}`);
 
   if (!note) return <div>Loading...</div>;
-
-  const title = note.noteTitle;
-  const noteHTML = note.noteHTML || "";
-  const parsedHTML = parse(noteHTML);
-  const parsedDate = parseISO(note.createdTime);
-  const formattedDate = format(parsedDate, "yyyy-MM-dd");
-
-  useDocTitle(`Reminded | ${title}`);
-
-  const handleEdit = () => onOpen();
 
   return (
     <section className="pb-5">
       <Card className="relative top-6 z-10 mx-auto mb-10 w-[95%] px-6 py-5">
-        {note.tags.length ? <CardTags card={note} /> : ""}
+        {note.tags.length ? <CardTags card={note.details} /> : ""}
         <div className="flex items-center justify-between">
-          <h1 className="my-4 text-2xl font-bold text-primary">{title}</h1>
+          <h1 className="my-4 text-2xl font-bold text-primary">
+            {note.noteTitle}
+          </h1>
           <Button
             color="secondary"
             size="sm"
             className="font-thin tracking-widest"
-            onPress={handleEdit}
+            onPress={() => onOpen()}
           >
             編輯
           </Button>
         </div>
         <span className="mb-3 text-sm text-third opacity-80">
-          {formattedDate}
+          {note.createdTime}
         </span>
         <Divider />
         <Spacer y={1} />
-        <div>{parsedHTML}</div>
+        <div>{note.createdTime}</div>
       </Card>
       <ModalEditor
         pageTitle="編輯筆記"
-        card={note}
+        card={note.details}
         isOpen={isOpen}
         onOpenChange={onOpenChange}
         onClose={onClose}
