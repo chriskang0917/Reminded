fcbaf4d8da730d29841738d5b2ace811ccefa59a
Chris <chriskang0917@gmail.com>
2024-01-01 08:12:20 +0800

    Fix: quick input display unexpected when in 875px width

--- src/components/Input/QuickInput/QuickInput.tsx
+++ src/components/Input/QuickInput/QuickInput.tsx
@@ -52,7 +52,7 @@ export const QuickInput = (props: QuickInputProps) => {
   };
 
   return (
-    <div className="relative w-full max-w-[465px] rounded-lg border-2 px-3 py-2">
+    <div className="relative mr-2 w-full rounded-lg border-2 px-3 py-2 md:max-w-[465px]">
       <MentionsInput
         autoFocus
         singleLine
